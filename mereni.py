ba64b53a97e5c62c5ae0f4e06af0902515824de5
VlastikJ <110531988+VlastikJ@users.noreply.github.com>
2022-12-09 10:55:29 +0100

    Add files via upload

---
+++ mereni.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+"""
+
+
+
+
+@author: Vlastimil
+"""
+#import novevyhodnoceni as vhd
+import numpy as np
+import matplotlib.pyplot as plt
+#import os.path as op
+from naberData import naberData
+from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TH1F,TString,TGraph
+import time
+#import ROOT
+# 1 SAMPLE = (12/245) us              
+
+rozsah = 50 #mV
+kolo_1 = 5
+kolo_2 = 9
+
+step = 1
+hist_r = TH1F("rozlozeni_amp"," ",rozsah*step,0,rozsah*step)
+
+sum = []
+cislo = 0
+ 
+capt =  1000
+starttime = time.time()
+endtime = 0.1 #pocet minut jak dlouho pojede mereni 
+while time.time() < starttime + endtime*60:
+#while cislo <10:
+    cislo = cislo + 1
+    napeti, n_samples = naberData('50 mV',capt,2)
+    napeti = -napeti
+    napeti = napeti.transpose()
+    name_napeti = "napeti_{}.root".format(cislo)
+    file2 = TFile(name_napeti, "recreate", "recreate")
+    print(napeti.shape)
+    for i in range(0,len(napeti)):
+        max_V = np.max(napeti[i])
+        sum.append(np.sum(napeti[i]))
+        for j in range(0,len(napeti[i])):
+            if napeti[i,j]>0:
+                print(napeti[i,j])
+            hist_r.Fill(napeti[i,j],1)
+        #hn.Write()
+    file2.Close()
+    max_I = np.max(sum)
+    n_bins = int(max_I*10)
+    hist_S = TH1F("suma_napeti"," ",n_bins,0,max_I)
+    for i in range(0,len(sum)):
+        hist_S.Fill(sum[i],1) 
+    name_hist = "hist_{}_{}_{}.root".format(capt,kolo_1,kolo_2)
+    file = TFile(name_hist, "recreate", "recreate")
+    hist_r.Write()
+    hist_S.Write()
+    file.Close()
+    start_pause = time.time()
---
+++ p5_mereni.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+"""
+
+
+
+
+@author: Vlastimil
+"""
+#import novevyhodnoceni as vhd
+import numpy as np
+import matplotlib.pyplot as plt
+#import os.path as op
+from naberData_5 import naberData_5
+import datetime as dt
+from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TH1F,TString
+import time
+#import ROOT
+# 1 SAMPLE = (12/245) us              
+
+rozsah = 10 #mV
+capt =  10000
+kolo_1 = 5
+kolo_2 = 10
+n_bins = 10000
+
+#0 jis for disable sec channel
+sec_C_range = '2 V'
+
+sum = []
+amp = []
+cislo = 0
+
+
+starttime = time.time()
+endtime = 2 #pocet minut jak dlouho pojede mereni 
+
+hist_r = TH1F("rozlozeni_amp"," ",n_bins,-rozsah,rozsah)
+while time.time() < starttime + endtime*60:
+#while cislo <10:
+    cislo = cislo + 1
+    napeti, n_samples = naberData_5('10 mV',capt,3,sec_C_range)
+    napeti = -napeti
+    napeti = napeti.transpose()
+    x = list(range(0,n_samples))
+    for i in range(0,len(napeti)):
+        
+        amp.append(np.max(napeti[i]))
+        sum.append(np.sum(napeti[i]))
+        for j in range(0,len(napeti[i])):            
+            hist_r.Fill(napeti[i,j],1)
+            
+    start_pause = time.time()
+    
+max_I = np.max(sum)
+min_I= np.min(sum)
+max_A = np.max(amp)
+min_A= np.min(amp)
+hist_A = TH1F("max_napeti"," ",n_bins,min_A-1,max_A+1)
+hist_S = TH1F("suma_napeti"," ",n_bins,min_I-10,max_I+10)
+for i in range(0,len(sum)):
+    hist_S.Fill(sum[i],1) 
+    
+for i in range(0,len(amp)):
+    hist_A.Fill(amp[i],1) 
+date = dt.datetime.now()
+name_hist = "hist_{}_{}_{}_{}.root".format(capt,kolo_1,kolo_2,date)
+file = TFile(name_hist, "recreate", "recreate")
+hist_r.Write()
+hist_S.Write()
+hist_A.Write()
+file.Close()
+    
